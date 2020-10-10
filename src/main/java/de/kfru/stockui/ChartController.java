7f68028413b951a1cb65282a38a6de6c1d4e56ef
JanMeckelholt <Jan_Meckelholt@web.de>
2020-10-10 22:00:02 +0200

    WebClientStockClient can not be imported

--- src/main/java/de/kfru/stockui/ChartController.java
+++ src/main/java/de/kfru/stockui/ChartController.java
@@ -1,11 +1,21 @@
 package de.kfru.stockui;
 
+import de.kfru.stockclient.WebClientStockClient;
 import javafx.fxml.FXML;
 import javafx.scene.chart.LineChart;
 import org.springframework.stereotype.Component;
+
+
 
 @Component
 public class ChartController {
     @FXML
     public LineChart<String, Double> chart;
+    private WebClientStockClient webClientStockClient;
+
+    public ChartController(WebClientStockClient webClientStockClient) {
+        this.webClientStockClient = webClientStockClient;
+    }
+
+
 }
